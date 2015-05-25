d28945c23a14ba87ccd58f747c18683d74187b4a
j <ciupis.jedrzej@gmail.com>
2015-05-25 18:08:53 +0200

    1st commit

---
+++ sym.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+/*int glosy[10]; //max 9 kandydatów
+
+int numer=1;
+
+class wyborca
+{
+private:
+    int numer_pop_kandydata;
+    int plec; // 1-facet, 0-babka
+public:
+    wyborca();
+    //friend void zliczanie_glosow();
+};
+
+class kandydat
+{
+private:
+    char* imie;
+    char* nazwisko;
+    int numer_kandydata;
+public:
+    kandydat();
+    ~kandydat();
+    friend void wyswietl_kandydatow();
+};
+
+void zliczanie_glosow()
+{
+
+
+}
+
+kandydat::kandydat()
+{
+    imie = new char[20];
+    cout << "Podaj imie: " << endl;
+    cin >> imie;
+    nazwisko = new char[20];
+    cout << "Podaj nazwisko: " << endl;
+    cin >> nazwisko;
+    numer_kandydata=numer;
+    numer++;
+}
+
+~kandydat()
+{
+    if(imie!=NULL) delete[] imie;
+    if(nazwisko!=NULL) delete[] nazwisko;
+    numer--;
+}
+
+wyborca::wyborca()
+{
+    cout << "Podaj swoja plec: (1-M, 0-K)";
+    cin >> plec;
+    cout << "Ktorego kandydata popierasz?" << endl;
+    wyswietl_kandydatow();
+    cin >> numer_pop_kandydata;
+    if(numer_pop_kandydata>9 || numer_pop_kandydata<1){
+             cout << "jeb sie";
+             glosy[0]++;
+    }
+    else glosy[numer_pop_kandydata]++;
+}
+
+void wyswietl_kandydatow();
+{
+    //
+}
+*/
+int main()
+{
+    cout << "dupa";
+    return 0;
+
+
+}
